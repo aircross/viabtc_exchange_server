d152b2a75a5009d01a72ab7a956861371b0b4558
mymixing <mymixing@163.com>
2017-12-05 18:12:34 +0800

    1

--- install_viabtc/install_viabtc.sh
+++ install_viabtc/install_viabtc.sh
@@ -1,10 +1,40 @@
 #!/usr/bin/env bash
 set -e
-basepath=$(cd `dirname $0`; pwd)
+basepath=$(cd `dirname $0`; pwd/..)
+echo $(pwd)
 
-function install(){
-    echo "install start $1 $2"
-}
-
-echo "process start $1 $2"
-install "dddd" "xxxx"
+#function install(){
+#    if [ $1 = "accesshttp" ]; then
+#        echo 1
+#    elif [ $1 = "accessws" ] ; then
+#        echo 1
+#    elif [ $1 = "alert" ] ; then
+#        echo 1
+#    elif [ $1 = "marketprice" ] ; then
+#        echo 1
+#    elif [ $1 = "matchengine" ] ; then
+#        echo 1
+#    elif [ $1 = "readhistory" ] ; then
+#        echo 1
+#    elif [ $1 = "all" ] ; then
+#        ./install_depends.sh
+#
+#    else
+#        echo "A command that cannot be understood"
+#    fi
+#}
+#
+#function uninstall(){
+#    echo 1
+#}
+#
+#if [ $1 = "install" ]; then
+#    install $2
+#elif [ $1 = "accessws" ] ; then
+#    uninstall $2
+#else
+#echo 222
+#fi
+#
+#echo "process start $1 $2"
+#install "dddd" "xxxx"
