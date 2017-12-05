1cf03fd82b0766be7fe245f4a1d1ecd0e4ffd1c2
mymixing <mymixing@163.com>
2017-12-05 15:28:33 +0800

    1

--- install_viabtc/install_mysql57.sh
+++ install_viabtc/install_mysql57.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
-yum install wget -y
-wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-rpm -ivh mysql57-community-release-el7-11.noarch.rpm
-yum install mysql-community-server mysql-community-client mysql-community-devel -y
+file="./mysql57-community-release-el7-11.noarch.rpm"
+
+if  [ ! -f "$file" ]; then
+  yum install wget -y
+  wget https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
+fi
+
+var="rpm -ivh mysql57-community-release-el7-11.noarch.rpm"
+echo $var
+#yum install mysql-community-server mysql-community-client mysql-community-devel -y
