4b01130c6b342242c221af2dd19bea66c21744ae
mymixing <mymixing@163.com>
2017-12-05 16:28:03 +0800

    1

--- install_viabtc/install_kafka.sh
+++ install_viabtc/install_kafka.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 set -e
-yum install wget tar java-1.8.0-openjdk -y
-
-fileUrl="http://mirrors.hust.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz"
-filename=$(basename $fileUrl)
-if [ ! -f "$filename" ]; then
-    wget $fileUrl
-else
-    tar -zxvf $filename
+destDir="/usr/local/kafka"
+if [ -d "$filename" ]; then
+    echo "the /usr/local/kafka directory already exists"
+    exit 1
 fi
+
+echo "123343dfdd"
+#yum install wget tar java-1.8.0-openjdk -y
+#
+#fileUrl="http://mirrors.hust.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz"
+#filename=$(basename $fileUrl)
+#if [ ! -f "$filename" ]; then
+#    wget $fileUrl
+#fi
+#tar -zxvf $filename
+
 
 #tar -zxvf kafka_2.11-1.0.0.tgz
 #mv kafka_2.11-1.0.0 /usr/local/kafka
