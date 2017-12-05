59b3465dd4b45ec429b74ad16840ec0c739863a9
mymixing <mymixing@163.com>
2017-12-05 17:00:33 +0800

    1

--- install_viabtc/install_kafka.sh
+++ install_viabtc/install_kafka.sh
@@ -3,25 +3,23 @@ set -e
 destDir="/usr/local/kafka"
 if [ -d "$destDir" ]; then
     echo "the '/usr/local/kafka' directory already exists"
-    exit 1
 fi
 
 yum install wget tar java-1.8.0-openjdk -y
 
 fileUrl="http://mirrors.hust.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz"
-filename=$(basename $fileUrl)
-if [ ! -f "$filename" ]; then
+compressFileName=$(basename $fileUrl)
+if [ ! -f "$compressFileName" ]; then
     wget $fileUrl
 fi
 
-filedir=$(dirname $filename)
-if [ ! -d "$filedir" ]; then
-    echo "22222222"
+fileExtName=${fileUrl##*.}
+uncompressDirName=$(basename $fileUrl)
+
+if [ ! -d "$uncompressDirName" ]; then
+    tar -zxvf $compressFileName
 fi
-#tar -zxvf $filename
 
-
-#tar -zxvf kafka_2.11-1.0.0.tgz
-#mv kafka_2.11-1.0.0 /usr/local/kafka
-#/usr/local/kafka/bin/zookeeper-server-start.sh -daemon /usr/local/kafka/config/zookeeper.properties
-#/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
+mv $uncompressDirName /usr/local/kafka
+/usr/local/kafka/bin/zookeeper-server-start.sh -daemon /usr/local/kafka/config/zookeeper.properties
+/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
