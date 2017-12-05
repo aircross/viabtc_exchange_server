c6242e10a21bbafe2fbb93706376f9353cf1cc32
mymixing <mymixing@163.com>
2017-12-05 18:24:28 +0800

    1

--- install_viabtc/install_depends.sh
+++ install_viabtc/install_depends.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -e
+basepath=$(cd `dirname $0`; pwd)
+projectpath=$(dirname $basepath)
+
 yum install librdkafka librdkafka-devel libcurl libcurl-devel libev libev-devel jansson jansson-devel mpdecimal mpdecimal-devel -y
 
-cd ../depends/hiredis
+cd $projectpath/depends/hiredis
 make -j4 && make install
-cd ../../utils
+cd $projectpath/network
+make -j4
+cd $projectpath/utils
 make -j4
--- install_viabtc/install_viabtc.sh
+++ install_viabtc/install_viabtc.sh
@@ -2,41 +2,42 @@
 set -e
 basepath=$(cd `dirname $0`; pwd)
 projectpath=$(dirname $basepath)
-echo $basepath
-echo $projectpath
 
-#function install(){
-#    if [ $1 = "accesshttp" ]; then
-#        echo 1
-#    elif [ $1 = "accessws" ] ; then
-#        echo 1
-#    elif [ $1 = "alert" ] ; then
-#        echo 1
-#    elif [ $1 = "marketprice" ] ; then
-#        echo 1
-#    elif [ $1 = "matchengine" ] ; then
-#        echo 1
-#    elif [ $1 = "readhistory" ] ; then
-#        echo 1
-#    elif [ $1 = "all" ] ; then
-#        ./install_depends.sh
-#
-#    else
-#        echo "A command that cannot be understood"
-#    fi
-#}
-#
-#function uninstall(){
-#    echo 1
-#}
-#
-#if [ $1 = "install" ]; then
-#    install $2
-#elif [ $1 = "accessws" ] ; then
-#    uninstall $2
-#else
-#echo 222
-#fi
-#
-#echo "process start $1 $2"
-#install "dddd" "xxxx"
+function install(){
+    if [ $1 = "accesshttp" ]; then
+        echo 1
+    elif [ $1 = "accessws" ] ; then
+        echo 1
+    elif [ $1 = "alertcenter" ] ; then
+        echo 1
+    elif [ $1 = "marketprice" ] ; then
+        echo 1
+    elif [ $1 = "matchengine" ] ; then
+        echo 1
+    elif [ $1 = "readhistory" ] ; then
+        echo 1
+    elif [ $1 = "all" ] ; then
+        processArray=("accesshttp" "accessws" "alertcenter" "marketprice" "matchengine" "readhistory")
+        ./install_depends.sh
+        echo "./install_depends.sh end"
+        for process in ${processArray[@]}
+        do
+            cd $projectpath/process
+            make -j4
+        done
+    else
+        echo "A command that cannot be understood"
+    fi
+}
+
+function uninstall(){
+    echo 1
+}
+
+if [ $1 = "install" ]; then
+    install $2
+elif [ $1 = "install" ] ; then
+    uninstall $2
+else
+    echo "不知道的指令"
+fi
