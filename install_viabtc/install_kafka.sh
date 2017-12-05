1e430262fa801b93375308ad0708f914dbca98af
mymixing <mymixing@163.com>
2017-12-05 16:10:55 +0800

    1

--- install_viabtc/install_kafka.sh
+++ install_viabtc/install_kafka.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 set -e
 yum install wget tar java-1.8.0-openjdk -y
-wget http://mirrors.hust.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz
-tar -zxvf kafka_2.11-1.0.0.tgz
-mv kafka_2.11-1.0.0 /usr/local/kafka
-/usr/local/kafka/bin/zookeeper-server-start.sh -daemon /usr/local/kafka/config/zookeeper.properties
-/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
+
+fileUrl="http://mirrors.hust.edu.cn/apache/kafka/1.0.0/kafka_2.11-1.0.0.tgz"
+if  [ ! -f "$file" ]; then
+  wget $fileUrl
+fi
+
+filename= $(basename $fileUrl)
+if [ ! -f "$filename" ]; then
+    echo 111
+else
+    echo 222
+fi
+#tar -zxvf kafka_2.11-1.0.0.tgz
+#mv kafka_2.11-1.0.0 /usr/local/kafka
+#/usr/local/kafka/bin/zookeeper-server-start.sh -daemon /usr/local/kafka/config/zookeeper.properties
+#/usr/local/kafka/bin/kafka-server-start.sh -daemon /usr/local/kafka/config/server.properties
