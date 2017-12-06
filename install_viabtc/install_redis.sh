bdb82c5fde4033da9b8e55db3f56b297a87d998f
mymixing <mymixing@163.com>
2017-12-06 12:48:30 +0800

    1

--- install_viabtc/install_redis.sh
+++ install_viabtc/install_redis.sh
@@ -2,3 +2,16 @@
 set -e
 basepath=$(cd `dirname $0`; pwd)
 projectpath=$(dirname $basepath)
+
+if [ ! -f "/etc/yum.repos.d/epel-7.repo" ]; then
+    yum install wget -y
+    wget -O /etc/yum.repos.d/epel-7.repo http://mirrors.aliyun.com/repo/epel-7.repo
+    yum clean all
+    yum makecache
+fi
+
+yum install redis -y
+redis-server $basepath/redis_master.conf
+redis-server $basepath/redis_sl1.conf
+redis-server $basepath/redis_sl2.conf
+redis-sentinel $basepath/sentinel-sy.conf
