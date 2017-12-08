47c348a5a982dd046a548bc84cbf8ef618543e8c
mymixing <mymixing@163.com>
2017-12-08 16:10:41 +0800

    1

--- sql/clear_trade_history.sh
+++ sql/clear_trade_history.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 MYSQL_HOST="rm-wz900j5wutr7o38di.mysql.rds.aliyuncs.com"
 MYSQL_USER="hotbit_make"
@@ -10,29 +11,29 @@ MYSQL_DB="hotbit_make_engine"
 for i in `seq 0 99`
 do
     echo "create table balance_history_$i"
-    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE balance_history_$i LIKE balance_history_example;"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE balance_history_$i;"
 done
 
 for i in `seq 0 99`
 do
     echo "create table order_history_$i"
-    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE order_history_$i LIKE order_history_example;"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE order_history_$i;"
 done
 
 for i in `seq 0 99`
 do
     echo "create table order_detail_$i"
-    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE order_detail_$i LIKE order_detail_example;"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE order_detail_$i;"
 done
 
 for i in `seq 0 99`
 do
     echo "create table deal_history_$i"
-    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE deal_history_$i LIKE deal_history_example;"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE deal_history_$i;"
 done
 
 for i in `seq 0 99`
 do
     echo "create table user_deal_history_$i"
-    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE user_deal_history_$i LIKE user_deal_history_example;"
+    mysql -h$MYSQL_HOST -u$MYSQL_USER -p$MYSQL_PASS $MYSQL_DB -e "TRUNCATE TABLE user_deal_history_$i;"
 done
